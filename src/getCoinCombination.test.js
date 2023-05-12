9fad49bb99dda15bceab9bc3db5d0480c5f3db24
viktorlyt <viktorlyt@gmail.com>
2023-05-12 16:59:53 +0300

    done

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -7,4 +7,20 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination)
       .toBeInstanceOf(Function);
   });
+
+  it('should return [1,0,0,0] if arg === 1', () => {
+    expect(getCoinCombination(1)).toEqual([1, 0, 0, 0]);
+  });
+
+  it('should return [1,1,0,0] if arg === 6', () => {
+    expect(getCoinCombination(6)).toEqual([1, 1, 0, 0]);
+  });
+
+  it('should return [2,1,1,0] if arg === 17', () => {
+    expect(getCoinCombination(17)).toEqual([2, 1, 1, 0]);
+  });
+
+  it('should return [0,0,0,2] if arg === 50', () => {
+    expect(getCoinCombination(50)).toEqual([0, 0, 0, 2]);
+  });
 });
